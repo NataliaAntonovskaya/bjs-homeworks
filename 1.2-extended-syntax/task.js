ecd2a3a4f9076f1d4ae50d5e57814a0c0fcbdbb0
NataliaAnt <buhnova@mail.ru>
2020-07-01 01:11:21 +0300

    task 1.2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,49 @@
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    'use strict';
+    let D = b ** 2 - 4 * a * c;
+    let x = [];
+    let x1, x2;
+    if (D > 0) {
+        x1 = (-b + Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a);
+        x2 = (-b - Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a);
+        x.push(x1, x2);
+    } else if (D === 0) {
+        x1 = -b / (2 * a);
+        x.push(x1);
+    }
+        
+    return x;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    let averageMark; 
+    let numberOfMarks = marks.length;    
+    if (numberOfMarks === 0) {
+        averageMark = 0;
+    } else if (numberOfMarks > 5) {
+        marks = marks.slice(0, 5);        
+    }
+    if (numberOfMarks > 0) {
+        let sumOfMarks = 0;
+        for (mark of marks) {
+            sumOfMarks += mark;
+        }
+        numberOfMarks = marks.length;
+        averageMark = sumOfMarks / numberOfMarks;
+    }        
+    
+    return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    const year = new Date().getFullYear();
+    let yearOfBirth = dateOfBirthday.getFullYear();
+    let age = year - yearOfBirth;
+    let result;
+    if (age >= 18) {
+        result = `Не желаете ли олд-фэшн, ${name}`;
+    } else {
+        result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    }
+    return result;
 }
